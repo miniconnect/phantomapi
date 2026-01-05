bf61da213dd55698aab2ca8af98fc0d6cce9692e
Dávid Horváth <horvath@webarticum.hu>
2026-01-05 10:38:24 +0100

    Extend the initial demo code

--- projects/core/src/main/java/hu/webarticum/phantomapi/core/Hello.java
+++ projects/core/src/main/java/hu/webarticum/phantomapi/core/Hello.java
@@ -10,6 +10,9 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import io.apicurio.datamodels.Library;
 import io.apicurio.datamodels.models.Document;
+import io.apicurio.datamodels.models.openapi.OpenApiPathItem;
+import io.apicurio.datamodels.models.openapi.OpenApiPaths;
+import io.apicurio.datamodels.models.openapi.v31.OpenApi31Document;
 
 public class Hello {
 
@@ -18,7 +21,45 @@ public class Hello {
             "info:\n" +
             "    title: Sample API\n" +
             "    description: Hello OpenAPI from PhantomAPI Core!\n" +
-            "    version: 0.1.0\n";
+            "    version: 0.1.0\n" +
+            "servers:\n" +
+            "  - url: '{scheme}://{host}:{port}/{path}'\n" +
+            "    variables:\n" +
+            "        scheme:\n" +
+            "            default: http\n" +
+            "            enum: [http, https]\n" +
+            "        host:\n" +
+            "            default: localhost\n" +
+            "            enum:\n" +
+            "              - localhost\n" +
+            "        port:\n" +
+            "            default: '3000'\n" +
+            "paths:\n" +
+            "    /users/{id}:\n" +
+            "        get:\n" +
+            "            parameters:\n" +
+            "              - name: id\n" +
+            "                in: path\n" +
+            "                required: true\n" +
+            "                schema:\n" +
+            "                    type: string\n" +
+            "            responses:\n" +
+            "                '200':\n" +
+            "                    description: OK\n" +
+            "                    content:\n" +
+            "                        application/json:\n" +
+            "                            schema:\n" +
+            "                                $ref: '#/components/schemas/User'\n" +
+            "components:\n" +
+            "    schemas:\n" +
+            "        User:\n" +
+            "            type: object\n" +
+            "            required: [id, name]\n" +
+            "            properties:\n" +
+            "                id:\n" +
+            "                    type: string\n" +
+            "                name:\n" +
+            "                    type: string\n";
 
     public static void hello() {
         ObjectMapper mapper = JsonMapper.builder(new YAMLFactory()).build();
@@ -28,8 +69,10 @@ public class Hello {
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
-        Document document = Library.readDocument(descriptionJsonNode);
+        OpenApi31Document document = (OpenApi31Document) Library.readDocument(descriptionJsonNode);
+        System.out.println(document.getClass());
         System.out.println(document.getInfo().getDescription());
+        System.out.println(document.getPaths().getItem("/users/{id}").getGet().getResponses().getItem("200").modelId());
     }
 
 }
